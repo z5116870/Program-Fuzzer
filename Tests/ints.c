77af9240480c4f348905a0c6707ed2a1f45bc4c3
Kevin Tra <kevin.tra@hotmail.com>
2021-08-04 16:15:51 +1000

    completed ints test binaries

--- Tests/ints.c
+++ Tests/ints.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //Testing for known ints
 //It allows users to pass in any number as the buffer size without checking
@@ -10,10 +11,7 @@ void vuln() {
     scanf("%d", &buffer_size);
     printf("Returning you a buffer %d bytes large\n", buffer_size);
     int *buffer = (int *)malloc(buffer_size);
-    for(int i = 0; i < buffer_size; i++){
-        buffer[i] = 0;
-        printf("Index %d: %d\n", i, buffer[i]);
-    }
+    memset(buffer, 0, buffer_size);
 }
 
 int main(int argc, char **argv) { vuln(); }
