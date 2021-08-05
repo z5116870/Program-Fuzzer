a3be6c21296f1f47fd325bc443efe0887798974b
Kevin Tra <kevin.tra@hotmail.com>
2021-08-05 16:38:47 +1000

    new ints test

--- Tests/ints2.c
+++ Tests/ints2.c
@@ -3,15 +3,17 @@
 #include <string.h>
 
 //Testing for known ints
-//It allows users to pass in any number as the buffer size without checking
-//No error checking if malloc succeeds
+//It allows users to pass in any number as the buffer index without checking
+//Large indexs corrupt, while negative indexes have strange outputs
 void vuln() {
-    int buffer_size;
-    printf("Enter a size for your buffer\n");
-    scanf("%d", &buffer_size);
-    printf("Returning you a buffer %d bytes large\n", buffer_size);
-    int *buffer = (int *)malloc(buffer_size);
-    memset(buffer, 0, buffer_size);
+    int storage[32];
+    for(int i = 0; i < 32; i++){
+        storage[i] = i;
+    }
+    int buffer_index;
+    printf("What index would you like to read from my buffer? (0-31)\n");
+    scanf("%d", &buffer_index);
+    printf("%d is at index %d\n", storage[buffer_index], buffer_index);
 }
 
 int main(int argc, char **argv) { vuln(); }
