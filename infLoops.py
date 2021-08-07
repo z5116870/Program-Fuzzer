dc4d4e134a0a179238ca0cb51e496debf9cb41a1
Roark <roark.menezes@gmail.com>
2021-08-07 18:36:35 +1000

    added infinte loop checks and arithmetic strategy to fuzzer.py

--- infLoops.py
+++ infLoops.py
@@ -3,84 +3,62 @@ from subprocess import Popen, PIPE
 import signal
 import time
 import os
-def time_out_handler(signum, frame):
-    print("Infinite loop detected. Exiting program.")
-    exit()
+timeout = 0
 
-binary = './Test/hang'
-testInput = 'Test/hang.txt'
-trace = Popen(['strace ' + binary + '<' + testInput], shell=True, stdout = open(os.devnull, 'wb'), stderr = PIPE)
-print('Running trace, pid: ', trace.pid)
+def infLoop(buf):
+	# COVERAGE BASED
+	with open('out.txt', 'w') as f:
+		syscalls = []
+		timeElapsed = time.time()
+		while buf.readable() and not timeout:
+			line = buf.readline()
+			if not line or line == b'+++ exited with 0 +++\n':
+				break
+			# Add to list of syscalls
+			syscalls.append(line)
+			# After 2 seconds has passed if strace still executing,
+			# Check to see if pattern can be found from last found syscalls
+			if(time.time() - timeElapsed > 0.2):
+				syscall = syscalls[-1] # last element
+				#print('callin')
+				timeElapsed = time.time()
+				#print(syscalls)
+				# Get a list of indexes that match the last recieved element
+				index_pos_list = []
+				index_pos = 0
+				while True:
+					try:
+						# Search for item in list from indexPos to the end of list
+						index_pos = syscalls.index(syscall, index_pos)
+						# Add the index position in list
+						index_pos_list.append(index_pos)
+						index_pos += 1
+					except ValueError:
+						start = time.time() # return to while loop for another 2 seconds
+						#print('All unique syscalls. Returning to trace...')
+						#print(syscall)
+						break
+				#print('list', index_pos_list)
 
-# TIMEOUT APPROACH
-signal.signal(signal.SIGALRM, time_out_handler)
-signal.alarm(10)
-
-with open(testInput) as f:
-	text = f.read()
-if not isinstance(text, bytearray):
-    text = bytes(text, 'utf-8')
-
-# COVERAGE BASED
-with open('out.txt', 'w') as f:
-	syscalls = []
-	start = time.time()
-	while trace.stderr.readable():
-		line = trace.stderr.readline()
-		if not line:
-			break
-		# Add to list of syscalls 
-		syscalls.append(line)
-
-		# write to file
-		f.write(str(line))
-		
-		# After 2 seconds has passed if strace still executing, 	
-		# Check to see if pattern can be found from last found syscalls
-		if(time.time() - start > 1):	
-			syscall = syscalls[-1] # last element
-			print(syscalls)
-			# Get a list of indexes that match the last recieved element
-			index_pos_list = []
-			index_pos = 0
-			while True:
-				try:
-					# Search for item in list from indexPos to the end of list
-					index_pos = syscalls.index(syscall, index_pos)
-					# Add the index position in list
-					index_pos_list.append(index_pos)
-					index_pos += 1
-				except ValueError:
-					start = time.time() # return to while loop for another 2 seconds
-					print('All unique syscalls. Returning to trace...')
-					print(syscall)
-					break
-			print('list', index_pos_list)
-			
-			# With this list of indexes, we can check for syscall loops	
-			for el in range(len(index_pos_list) - 1):
-				prev = index_pos_list[el]
-				next = index_pos_list[el + 1] 
-
-				i = 0
-				check1 = syscalls[prev + i]
-				check2 = syscalls[next + i]
-
-				# If we find 10 loops in a row, probable infinite loop	
-				loops = 0
-				while(check1 ==  check2):
-					if(loops > 10):
-						print("Infinite loop detected. Exiting program.")
-						exit()
-					i += 1
-					# If we loop back to the beginning 
-					print(prev, next, i)
-					if(prev + i == next):
-						prev = next
-						next = next + i
-						loops += 1
-						i = 0
-						continue
-					check1 = syscalls[prev + i]
-					check2 = syscalls[next + i]
-
+				# With this list of indexes, we can check for syscall loops
+				for el in range(len(index_pos_list) - 1, -1, -1):
+					# We dont need to check the actual syscalls
+					# Check distance between each index. If a pattern can be found for > 10
+					# Then it is an infinite loop. We start from the last element (most recently recieved)
+					# and count back
+					start = el
+					posA = index_pos_list[start]
+					if(start == 0):
+						print('reached 0')
+						return 1
+					posB = index_pos_list[start - 1]
+					dist = posA - posB
+					loop = 0
+					while posA - posB == dist:
+						if(loop > 10):
+							return 1337
+						loop += 1
+						start -= 1
+						posA = index_pos_list[start]
+						posB = index_pos_list[start - 1]
+	return 1
