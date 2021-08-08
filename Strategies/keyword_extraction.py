2ab945a125ec411aaa6b3705faf13dcaaa9c916c
Roark <roark.menezes@gmail.com>
2021-08-08 12:02:51 +1000

    repeated parts: Checks PDF

--- Strategies/keyword_extraction.py
+++ Strategies/keyword_extraction.py
@@ -1,174 +1,255 @@
-# from pwn import *
-import sys
-from subprocess import Popen, PIPE
-#from getFileType import FileType, getFileType
-from Strategies.getFileType import FileType, getFileType
-from bs4 import BeautifulSoup as BS
-from PIL import Image, ExifTags
-import piexif
-import pyexiv2
+import json
+import random
+from Strategies.getFileType import FileType, CSV_DELIMITER
+from xmljson import badgerfish as bf
+# https://pypi.org/project/xmljson/
+from xml.etree.ElementTree import Element, tostring, fromstring
 import re
-import base64
-import os,binascii
+from exif import Image
+# https://pypi.org/project/exif/
+from PyPDF2 import PdfFileReader, PdfFileWriter
+# https://pypi.org/project/PyPDF2/
+from makeelf.elf import *
+# https://github.com/v3l0c1r4pt0r/makeelf
+import os
 
-def runFuzzedInput(text, binary, inputtype):
-	proc = Popen([binary], shell=True, stdin = PIPE, stdout = PIPE, stderr = PIPE)
-	if(inputtype == FileType.jpeg):
-		try:
-			output, error = proc.communicate(text.tobytes())
-		except:
-			output, error = proc.communicate(text)
-	else:
-		output, error = proc.communicate(bytes(text, 'utf-8'))
-	return(proc.returncode, error)
+RUN_COUNT = 0
 
-# JSON
-'''
-if(inputtype == TYPE_JSON):
-	payload += '{'
-	# Repeat first key/val pair
-	with open(testInput) as f:
-		text = f.read()
-		res = 0
-		for i in range(0, len(text)):
-			if text[i] == ',':
-				res = i + 1
-				break
-		# add the repeat
-		payload += text[1:res] * 2
-		# put the rest of the payload
-		payload += text[res:]
-	print(payload)
-'''
-# CSV
-def repeatedParts(testInput, inputtype):
+def keyword_extract(data, delimiter):
+    dictionary = []
+    file_data = []
+    for line in data:
+        dictionary += line.strip().split(delimiter)
+        file_data += line.split(delimiter)
 
-	# Fuzz depending on input type
-	payload = ''
-	payloads = []
-	if(inputtype == FileType.csv or inputtype == FileType.json):
-		with open(testInput) as f:
-			text = f.read()
-		# Header stays intact
-		i = 1
-		while i < len(text):
-			for x in range(1, len(text) - i):
-				string = text[i:i+x]
-				payload += text[0:i] + string*14 + text[i:]
-				payloads.append(payload)
-				payload = ''
-			i += 1
+    return dictionary, file_data
 
-	if(inputtype == FileType.xml):
-		with open(testInput) as f:
-			soup = BS(f, features='lxml')
-		# First, find all the tags in the xml
-		tags = []
-		for tag in soup.find_all(True):
-			print(tag.name)
-			tags.append(tag.name)
+def xmlfile_to_json(data_str):
+    json_text = bf.data(fromstring(data_str))
+    json_text = json.dumps(json_text)
+    return json.loads(json_text)
 
-		# Method 1, repeat everything between the tags
-		for tag in tags:
-			text = str(soup)
-			x = text.find(tag)
+# reference: https://www.geeksforgeeks.org/turning-a-dictionary-into-xml-in-python/
+def json_dict_to_xml(dict):
+    elem = Element('root')
 
-			# Get the entire tag and contents
-			xmlstr = str(soup.find(tag))
+    for key, val in dict.items():
+        child = Element(key)
+        child.text = str(val)
+        elem.append(child)
 
-			# add the repeated text just after the tag
-			y = len(xmlstr)
-			index = x + y
-			payload = text[:index] + xmlstr*2 + text[index:]
+    return tostring(elem).decode('utf-8')
 
-			# add it to the payloads
-			print(payload)
-			payloads.append(payload)
-			payload = ''
+def add_xml_prologue(data_str):
+    # Use bad encoding that contains security vulns if read as html in browsers
+    data = '<?xml version="1.0" encoding="JIS_C6226-1983"?>\n' + data_str
+    return data
 
-		# Method 2, repeat the tag text, within the tag itself
-		for tag in tags:
-			tagtext = soup.find(tag).text
-			print('-------')
-			print(tagtext)
-	if(inputtype == FileType.plaintext):
-		payload = text*10000
-		payloads.append(payload)
+def xml_remove_closing_tags(data_str):
+    new_str = re.sub('</.*?>', '', data_str)
+    return new_str
 
-	if(inputtype == FileType.jpeg):
-		# Using Pillow (PIL)
-		print('jpeg!!')
-		img = Image.open(testInput)
-		print(img.info)
-		keys = []
-		values = []
-		for k, v in img.info.items():
-			#print(k, ':', v)
-			keys.append(k)
-			values.append(v)
-		# Repeat each key, value
-		for i in range(len(keys)):
-			for j in range(10):
-				img.info.__setitem__(keys[i]*j, values[i]*j*2)
-				#print(img.info)
-				payloads.append(img)
+def xml_format_str_insert(data_str):
+    print(data_str)
+    new_str = re.sub('\"(.*?)\"', "'%n'", data_str)
+    return new_str
 
-		# Using b64 method
-		with open(testInput, "rb") as f:
-			b64str = base64.b64encode(f.read())
-		i = 0
-		for c in str(b64str):
-			if c == '/':
-				# generate random 8 byte hex number and insert
-				# after every slash
-				rpt = binascii.b2a_hex(os.urandom(8))
-				payload = b64str[:i] + rpt + b64str[i:]
-				# Assert b64 string is 4 byte aligned
-				if(len(payload) % 4 != 0):
-					payload += b'='
-				payload = base64.b64decode(b"data:image/jpeg;base64," + payload)
-				payloads.append(payload)
-			i += 1
-		print(i)
-		print(b64str)
-	return payloads
+def deeply_nested_xml_no_closing_brace():
+    data_str = ""
+    for i in range(0, 100):
+        data_str += "\t"*i + "<a>\n"
+    return data_str
 
-def run(binary, testInput):
-	print("making fuzzed inputs...")
-	inputtype = getFileType(testInput)
-	payloads = repeatedParts(testInput, inputtype)
-	print("Done.")
-	badpload = []
-	codes = []
-	errors = []
-	crashes = 0
-	# p = process(binary)
-	print("running fuzzed inputs...: " + binary)
-	for payload in payloads:
-		retCode, error = runFuzzedInput(payload, binary, inputtype)
-		if(retCode != 0):
-			crashes += 1
-			badpload.append(payload)
-			codes.append(retCode)
-			errors.append(error)
-	printStats(crashes, badpload, codes, errors)
 
-def printStats(crashes, badpload, codes, errors):
-	print("---STATS---")
-	print("CRASHES: ", crashes)
-	print("CAUGHT PAYLOADS:")
-	i = 0
-	x = 0
-	#for pload in badpload:
-		#print(x,': ', pload)
-		#x += 1
+def json_keyword_extract(data):
+    return list(data.keys())
 
-	# print only unique codes
-	u = []
-	for i in codes:
-		if i not in u:
-			u.append(i)
-	print("CAUGHT CODES: ", u)
-	print("CAUGHT ERRORS: ", errors)
+def json_mutate_input(data, dictionary, dict=False):
+    random_keyword = dictionary[random.randint(0, len(dictionary)-1)]
+    random_json_location = random.randint(0, len(list(data.keys()))-1)
+    random_json_keyword = ""
+    i = 0
+    for key in data.keys():
+        if i == random_json_location:
+            random_json_keyword = key
+        i += 1
+    # while (random_keyword == random_json_keyword):
+    #     random_json_location = random.randint(0, len(list(data.keys()))-1)
+    #     i = 0
+    #     for key in data.keys():
+    #         if i == random_json_location:
+    #             random_json_keyword = key;
+    #         i += 1
 
-#run(sys.argv[1], sys.argv[2])
+    random_keyword_value = data[random_keyword]
+    random_json_keyword_value = data[random_json_keyword]
+
+    # replace random_json_keyword with random_keyword
+    data.pop(random_keyword)
+    if (random_keyword != random_json_keyword):
+        data.pop(random_json_keyword)
+
+    data[random_keyword] = random_json_keyword_value
+    if (random_keyword != random_json_keyword):
+        data[random_json_keyword] = random_keyword_value
+
+    if dict == True:
+        return data
+
+    return json.dumps(data)
+
+def csv_keyword_extract(data):
+    return keyword_extract(data, CSV_DELIMITER)
+
+def csv_mutate_input(data, dictionary):
+    random_keyword_index = random.randint(0, len(dictionary)-1)
+    random_csv_keyword_index = random.randint(0, len(data)-1)
+
+    # 1 in 4 runs don't do a random mutation
+    global RUN_COUNT
+    if (RUN_COUNT % 4 == 0):
+        random_keyword_index = 1
+
+    if (len(data) == 1):
+        data[0] = dictionary[random_keyword_index]
+
+    words_to_replace = random.randint(0, len(data) -1)
+    for i in range(0, words_to_replace):
+        if ("\n" in data[random_csv_keyword_index]):
+            data[random_csv_keyword_index] = dictionary[random_keyword_index] + "\n"
+        else:
+            data[random_csv_keyword_index] = dictionary[random_keyword_index]
+
+        random_keyword_index = random.randint(0, len(dictionary)-1)
+        random_csv_keyword_index = random.randint(0, len(data)-1)
+
+    new_output = ""
+    for word in data:
+        if (new_output != "" and new_output[-1] == "\n") or new_output == "":
+            new_output += word
+        else:
+            new_output += ("," + word)
+    return new_output
+
+# def jpegMutateInput(file, dictionary):
+#     # open image
+
+def keyword_fuzzing(input, type = None, mutations = None, output_file = None):
+    global RUN_COUNT
+    mutation_str = mutations
+    if mutations is None:
+        mutation_str = "random"
+    # print("Fuzzing file:", input, "with type", type, "Number of keyword swaps:", mutation_str, "\n")
+    if (type == FileType.pdf or type == FileType.elf or type == FileType.jpeg):
+        file = open(input, 'rb')
+    else:
+        file = open(input, 'r')
+    fuzzed_output = ""
+    if type == FileType.json:
+        data = json.load(file)
+        dictionary = json_keyword_extract(data)
+        dictionary += ["%n"]
+        fuzzed_output = json_mutate_input(data, dictionary)
+    elif type == FileType.csv:
+        dictionary, file_data = csv_keyword_extract(file)
+        dictionary += ["%n"]
+        fuzzed_output = csv_mutate_input(file_data, dictionary)
+    elif type == FileType.elf:
+        # Need to a file to save in if we want a modified elf
+        if (output_file == None):
+            fuzzed_output = ""
+        else:
+            # Mutate elf header info such to craft bad ELF files
+            file.close()
+            elf = ELF.from_file(input)
+
+            # get headers
+            elf = elf.Elf
+            elf.append_section('BAD SECTION', "???%n%n%n", 0xdeadbeaf)
+            # args: section_id, addr, mem_size. GIVE invalid mem_size and addr
+            elf.append_segment(0, 0xdeadbeaf, -100000)
+            new_elf = os.open(output_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+            os.write(new_elf, bytes(elf))
+            os.close(new_elf)
+
+    elif type == FileType.pdf:
+        # Need to a file to save in if we want a modified pdf
+        if (output_file == None):
+            fuzzed_output = ""
+        else:
+            # read metadata extract out keywords
+            pdf = PdfFileReader(file)
+            pdf_info = pdf.getDocumentInfo()
+            pdf_writer = PdfFileWriter()
+            dictionary = []
+            if '/Keywords' in pdf_info:
+                dictionary = pdf_info['/Keywords']
+            # modify metadata
+            if (RUN_COUNT % 4 == 0):
+                pdf_info.title = "%n%n%n"
+                pdf_info.author = dictionary[random.randint(0, len(dictionary)-1)]
+                pdf_info.creator = dictionary[random.randint(0, len(dictionary)-1)]
+            else:
+                pdf_writer.addPage(pdf_info.getPage(0))
+                pdf_writer.addMetadata({"secret": "%n%n%n%n%n"})
+                pdf_writer.insertBlankPage(100, 100, 1);
+                pdf_writer.insertBlankPage(100, 100, 2);
+                pdf_writer.insertBlankPage(100, 100, 3);
+                with open(output_file, "wb") as new_pdf:
+                        pdf_writer.write(new_pdf)
+
+    elif type == FileType.xml:
+        data_str = file.read()
+        # convert xml to json
+        json_dict = xmlfile_to_json(data_str)
+        # use json mutation plus custom dictionary
+        dictionary = json_keyword_extract(json_dict)
+        dictionary += ["%n"]
+        fuzzed_output_dict = json_mutate_input(json_dict, dictionary, dict=True)
+        # convert json back to xml
+        fuzzed_output = json_dict_to_xml(fuzzed_output_dict)
+        if (RUN_COUNT % 4 == 0):
+            fuzzed_output = xml_remove_closing_tags(fuzzed_output)
+            fuzzed_output += deeply_nested_xml_no_closing_brace()
+        elif (RUN_COUNT % 4 == 1):
+            # add weird xml prologue
+            fuzzed_output = add_xml_prologue(fuzzed_output)
+        elif (RUN_COUNT % 4 == 2):
+            fuzzed_output = xml_format_str_insert(data_str)
+    elif type == FileType.plaintext:
+        dictionary, file_data = keyword_extract(file, " ")
+        # add in special words into dictionary
+        dictionary += ["longlonglongword", "%n","Admin", "admin", "ADMIN", "password", "sudo"]
+        # replace words with same strategy as csv
+        fuzzed_output = csv_mutate_input(file_data, dictionary)
+        print(fuzzed_output)
+    elif type == FileType.jpeg:
+        # Need to a file to save in if we want a modified jpeg
+        if (output_file != None):
+            fuzzed_output = ""
+        else:
+            # JPEGS don't really have keywords BUT we can
+            # modify metadata info if it exits
+            jpeg_img = Image(file)
+            if (jpeg_img.has_exif):
+                if (RUN_COUNT % 4 == 0):
+                    jpeg_img.delete_all()
+                else:
+                    try:
+                        for key in dir(jpeg_img):
+                            jpeg_img.set(key, -1)
+                    except:
+                        print("JPEG metadata update failed")
+                with open(output_file, "wb") as new_img:
+                        new_img.write(jpeg_img.get_file())
+    else:
+        print("Type not supported")
+        for line in file:
+            fuzzed_output += line
+    file.close()
+    RUN_COUNT =  RUN_COUNT + 1
+    if (output_file != None and (type != FileType.elf  or type != FileType.pdf or type != FileType.jpeg)):
+        output = open(output_file, "w")
+        output.write(fuzzed_output)
+        output.close()
+    return fuzzed_output
--- Strategies/repeatedParts.py
+++ Strategies/repeatedParts.py
@@ -1,14 +1,32 @@
 # from pwn import *
 import sys
 from subprocess import Popen, PIPE
-from getFileType import FileType, getFileType
+#from getFileType import FileType, getFileType
+from Strategies.getFileType import FileType, getFileType
 from bs4 import BeautifulSoup as BS
+from PIL import Image, ExifTags
+import piexif
+import pyexiv2
 import re
+import base64
+import os,binascii
+import PyPDF2
+import random
+import string
 
-def runFuzzedInput(text, binary):
+randString = string.ascii_letters + string.punctuation
+PDF_KEYWORDS = ['/CreationDate', '/Creator', '/Keywords', '/Producer', '/Title']
+
+def runFuzzedInput(text, binary, inputtype):
 	proc = Popen([binary], shell=True, stdin = PIPE, stdout = PIPE, stderr = PIPE)
-	output, error = proc.communicate(bytes(text, 'utf-8'))
-	return(proc.returncode)
+	if(inputtype == FileType.jpeg or inputtype == FileType.pdf):
+		try:
+			output, error = proc.communicate(text.tobytes())
+		except:
+			output, error = proc.communicate(text)
+	else:
+		output, error = proc.communicate(bytes(text, 'utf-8'))
+	return(proc.returncode, error)
 
 # JSON
 '''
@@ -34,23 +52,23 @@ def repeatedParts(testInput, inputtype):
 	# Fuzz depending on input type
 	payload = ''
 	payloads = []
-	with open(testInput) as f:
-		text = f.read()
 	if(inputtype == FileType.csv or inputtype == FileType.json):
-			# Header stays intact
-			i = 1
-			while i < len(text):
-				for x in range(1, len(text) - i):
-					string = text[i:i+x]
-					payload += text[0:i] + string*14 + text[i:]
-					payloads.append(payload)
-					payload = ''
-				i += 1
+		with open(testInput) as f:
+			text = f.read()
+		# Header stays intact
+		i = 1
+		while i < len(text):
+			for x in range(1, len(text) - i):
+				string = text[i:i+x]
+				payload += text[0:i] + string*14 + text[i:]
+				payloads.append(payload)
+				payload = ''
+			i += 1
 
 	if(inputtype == FileType.xml):
 		with open(testInput) as f:
 			soup = BS(f, features='lxml')
-		# First, find all the tags in the xml		
+		# First, find all the tags in the xml
 		tags = []
 		for tag in soup.find_all(True):
 			print(tag.name)
@@ -62,13 +80,13 @@ def repeatedParts(testInput, inputtype):
 			x = text.find(tag)
 
 			# Get the entire tag and contents
-			xmlstr = str(soup.find(tag))		
+			xmlstr = str(soup.find(tag))
 
 			# add the repeated text just after the tag
 			y = len(xmlstr)
 			index = x + y
 			payload = text[:index] + xmlstr*2 + text[index:]
-			
+
 			# add it to the payloads
 			print(payload)
 			payloads.append(payload)
@@ -82,6 +100,75 @@ def repeatedParts(testInput, inputtype):
 	if(inputtype == FileType.plaintext):
 		payload = text*10000
 		payloads.append(payload)
+
+	if(inputtype == FileType.jpeg):
+		# Using Pillow (PIL)
+		print('jpeg!!')
+		img = Image.open(testInput)
+		print(img.info)
+		keys = []
+		values = []
+		for k, v in img.info.items():
+			#print(k, ':', v)
+			keys.append(k)
+			values.append(v)
+		# Repeat each key, value
+		for i in range(len(keys)):
+			for j in range(10):
+				img.info.__setitem__(keys[i]*j, values[i]*j*2)
+				#print(img.info)
+				payloads.append(img)
+
+		# Using b64 method
+		with open(testInput, "rb") as f:
+			b64str = base64.b64encode(f.read())
+		i = 0
+		for c in str(b64str):
+			if c == '/':
+				# generate random 8 byte hex number and insert
+				# after every slash
+				rpt = binascii.b2a_hex(os.urandom(8))
+				payload = b64str[:i] + rpt + b64str[i:]
+				# Assert b64 string is 4 byte aligned
+				if(len(payload) % 4 != 0):
+					payload += b'='
+				payload = base64.b64decode(b"data:image/jpeg;base64," + payload)
+				payloads.append(payload)
+			i += 1
+		print(i)
+		print(b64str)
+
+	if(inputtype == FileType.pdf):
+		# read input as PDF file
+		pdf_writer = PyPDF2.PdfFileWriter()
+		mDict = {}
+		with open(testInput, 'rb') as f:
+			pdf = PyPDF2.PdfFileReader(f)
+			# get dictionary of doc info values
+			info = pdf.getDocumentInfo()
+
+		for i in range(100):
+			# repeat metadata
+			for k, v in info.items():
+				print(k, ':', v)
+				# repeat existing keywords
+				mDict.__setitem__(k, v*5)
+
+			# repeat random data
+			for kword in PDF_KEYWORDS:
+				if kword not in mDict:
+					v = ''.join(random.choice(randString) for x in range(8))
+					mDict.__setitem__(kword, v*5)
+			print(mDict)
+			pdf_writer.addMetadata(mDict)
+			with open('out.pdf', "wb") as f:
+				pdf_writer.write(f)
+
+			with open('out.pdf', 'rb') as f:
+				payload = f.read()
+			payloads.append(payload)
+
+		print(payload)
 	return payloads
 
 def run(binary, testInput):
@@ -91,26 +178,28 @@ def run(binary, testInput):
 	print("Done.")
 	badpload = []
 	codes = []
+	errors = []
 	crashes = 0
 	# p = process(binary)
 	print("running fuzzed inputs...: " + binary)
 	for payload in payloads:
-		retCode = runFuzzedInput(payload, binary)
+		retCode, error = runFuzzedInput(payload, binary, inputtype)
 		if(retCode != 0):
 			crashes += 1
 			badpload.append(payload)
 			codes.append(retCode)
-	printStats(crashes, badpload, codes)
+			errors.append(error)
+	printStats(crashes, badpload, codes, errors)
 
-def printStats(crashes, badpload, codes):
+def printStats(crashes, badpload, codes, errors):
 	print("---STATS---")
 	print("CRASHES: ", crashes)
 	print("CAUGHT PAYLOADS:")
 	i = 0
 	x = 0
-	for pload in badpload:
-		print(x,': ', pload)
-		x += 1
+	#for pload in badpload:
+		#print(x,': ', pload)
+		#x += 1
 
 	# print only unique codes
 	u = []
@@ -118,5 +207,6 @@ def printStats(crashes, badpload, codes):
 		if i not in u:
 			u.append(i)
 	print("CAUGHT CODES: ", u)
+	print("CAUGHT ERRORS: ", errors)
 
-run(sys.argv[1], sys.argv[2])
+#run(sys.argv[1], sys.argv[2])
